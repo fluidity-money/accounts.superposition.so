088baa98bf30fbff9cecd8db46ec6a1ce297a426
bayge <alex@fluidity.money>
2025-12-15 18:48:21 +0900

    Use the correct nonce management for the packing

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,6 +6,7 @@ package graph
 
 import (
 	"context"
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"log/slog"
@@ -147,14 +148,18 @@ func (r *mutationResolver) RequestSecret(ctx context.Context, eoaAddr string, no
 	if sig[64] == 27 || sig[64] == 28 {
 		sig[64] -= 27
 	}
+	b = make([]byte, 4)
+	if _, err := binary.Encode(b, binary.BigEndian, nonce); err != nil {
+		return "", fmt.Errorf("encoding: %v", err)
+	}
 	pubKey, err := ethCrypto.SigToPub(
 		ethCrypto.Keccak256(
 			[]byte("\x19Ethereum Signed Message:\n"),
 			// Length of the private key in hex + size of the encoded u64 as a big
 			// endian number:
-			[]byte(strconv.Itoa(64 + 8)),
+			[]byte(strconv.Itoa(64+8)),
 			[]byte(hex.EncodeToString(r.AccPubKey[:])),
-			[]byte(strconv.Itoa(int(nonce))),
+			[]byte(hex.EncodeToString(b)),
 		),
 		sig[:],
 	)
