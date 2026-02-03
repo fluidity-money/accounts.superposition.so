c23d125e50432a1df15532f9d5d7417e7ebbd52f
bayge <alex@fluidity.money>
2026-02-03 19:07:49 +0900

    More efficient migration and new address

--- README.md
+++ README.md
@@ -45,7 +45,7 @@ binary for deployment.
 ## Deployments
 
 `0xb838e2C1C9e525dFE18D35cd906aEe141ce9CfC2` is the proxy of the accounts factory.
-`0xed3e57f19789e27b4fadc74696ad3e5adc10bc8c` is the implementation.
+`0x02dca5820d1afae2bb53be344276da9d0cec0f9f` is the implementation.
 
 `0x4B4e7127A5Ae64D7c96997B2c23BdB09C4d47d3F` is the implementation of the 9lives Authority
 address. `0x0e3CD9653D9d9610281551d8E3C98035215EA18e` is the proxy.
--- libaccounts/src/entry_migrate.rs
+++ libaccounts/src/entry_migrate.rs
@@ -1,21 +1,21 @@
-use bobcat_sdk::{
-    entry::msg_sender,
-    maths::U,
-    storage::{storage_load, storage_store},
-};
+use bobcat_sdk::{entry::msg_sender, maths::U, storage::storage_store};
 
 use crate::{storage, SLOT_IMPL};
 
 pub fn entry_migrate(ed_key: &U, evm_owner: &U, impl_addr: &U, authority_addr: &U) -> usize {
-    let migration_level = storage_load(&SLOT_IMPL);
+    // Despite whatever we're doing, we will always change the
+    // implementation. So this must be set:
+    assert!(impl_addr.is_some());
+
+    let migration_level = storage::version::get();
     let mut is_first_time = false;
 
-    // Duplicate storage writes here aren't a big deal thanks to the Stylus trie:
+    // Duplicate storage writes here aren't a big deal thanks to the Stylus tree:
 
     /* ~~~~ MIGRATION LEVEL 0: ~~~~
      * 1. Sets up the owner of the contract, including the slots.
      * 2. Migrates to the new storage slot for the implementation.
-    */
+     */
 
     if migration_level == U::ZERO {
         assert!(ed_key.is_some());
@@ -24,8 +24,6 @@ pub fn entry_migrate(ed_key: &U, evm_owner: &U, impl_addr: &U, authority_addr: &
         storage::ed25519_slot::set(&U::ZERO, ed_key);
         storage::ed25519_count::set(&U::ONE);
         storage::ethereum_owner::set(evm_owner);
-        assert!(impl_addr.is_some());
-        storage_store(&SLOT_IMPL, impl_addr);
         is_first_time = true;
     }
 
@@ -35,19 +33,25 @@ pub fn entry_migrate(ed_key: &U, evm_owner: &U, impl_addr: &U, authority_addr: &
      */
 
     if migration_level <= U::ONE {
-        // If we've already migrated this contract, then we need to also check
-        // the migrator to prevent abuse:
-        if storage::ethereum_owner::get().is_some() && !is_first_time {
-            assert_eq!(
-                storage::ethereum_owner::get(),
-                msg_sender().into(),
-                "bad migrator"
-            );
-        }
         assert!(impl_addr.is_some());
-        storage_store(&SLOT_IMPL, impl_addr);
         storage::authority::set(authority_addr);
     }
+
+    // If we've already migrated this contract, then we need to also check
+    // the migrator to prevent abuse. This should unwind the tree so it's
+    // safe to have here, at the end:
+    if !is_first_time {
+        assert_eq!(
+            storage::ethereum_owner::get(),
+            msg_sender().into(),
+            "bad migrator"
+        );
+    }
+
+    // Despite the way this function works as a migration function, we allow
+    // users to upgrade themselves to any implementation they want:
+
     storage::version::set(&U::from(2u32));
+    storage_store(&SLOT_IMPL, impl_addr);
     0
 }
