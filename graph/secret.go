de48db46d92e8343d6916fb6a4cdd623745027f2
bayge <alex@fluidity.money>
2026-02-09 12:32:08 +0900

    Remove argon2

--- graph/secret.go
+++ graph/secret.go
@@ -4,8 +4,6 @@ import (
 	"crypto/rand"
 	"fmt"
 	"math/big"
-
-	"golang.org/x/crypto/argon2"
 )
 
 //57896044618658097711785492504343953926418782139537452191302581570759080747168
@@ -15,10 +13,6 @@ var secpMaxS = new(big.Int).SetBits([]big.Word{
 	18446744073709551615,
 	9223372036854775807,
 })
-
-func MakeKey(password, salt []byte) []byte {
-	return argon2.IDKey(password, salt, 1, 64*1024, 4, 32)
-}
 
 func makeSecret() (secret []byte) {
 	secret = make([]byte, 32)
