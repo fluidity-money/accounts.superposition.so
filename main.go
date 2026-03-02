49e2a569dabf19284d2eb1a8698b14d3e0d435f5
bayge <alex@fluidity.money>
2026-03-02 13:06:28 +0900

    Reenable the playground

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
 
 	"github.com/vektah/gqlparser/v2/ast"
 
@@ -219,6 +220,7 @@ func main() {
 		Cache: lru.New[string](100),
 	})
 	http.Handle("/", authMiddleware{db, srv, adminSecret})
+	http.Handle("/playground", playground.Handler("GraphQL playground", "/"))
 	switch typ := os.Getenv(EnvBackendType); typ {
 	case "lambda":
 		lambda.Start(httpadapter.NewV2(http.DefaultServeMux).ProxyWithContext)
