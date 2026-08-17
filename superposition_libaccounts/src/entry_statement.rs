f58563b718101360797b57fa3a611783698925ee
bayge <alex@fluidity.money>
2026-08-17 14:23:33 +0800

    Ask the users to have a prelude for signing the message

--- superposition_libaccounts/src/entry_statement.rs
+++ superposition_libaccounts/src/entry_statement.rs
@@ -1,10 +1,18 @@
 use crate::{Sig, StatementArgs, storage};
 
-use bobcat_sdk::{maths::U, precompiles::superposition::edphverify_pre};
+use bobcat_sdk::{entry::contract_address, maths::U, precompiles::superposition::edphverify_pre};
+
+use array_concat::concat_arrays;
 
 pub fn entry_statement(args: StatementArgs, sig: Sig) -> usize {
+    let content = borsh::to_vec(&args).unwrap();
+    let mut contract_addr: [u8; 40] = [0u8; 40];
+    const_hex::encode_to_slice(contract_address(), &mut contract_addr).unwrap();
+    let msg: [u8; 90] = concat_arrays!(*b"Superposition Accounts statement for ", contract_addr);
+    let mut msg = msg.to_vec();
+    msg.extend(content);
     assert!(edphverify_pre(
-        &borsh::to_vec(&args).unwrap(),
+        &msg,
         storage::ed25519_slot::get(&U::ZERO),
         sig.0
     ));
